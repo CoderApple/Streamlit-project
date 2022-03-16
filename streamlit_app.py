4b4553d266e6f4aeaf6c46ce93494d8be02f3c82
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-03-16 06:02:14 +0530

    expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ col1, col2 = st.columns(2);
 if(Semester == 1):
     st.header("Enter your Marks!")
     with col1:
-        st.expander("Theory Marks")
-        st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
+        with st.expander("Theory Marks"):
+            st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
 
