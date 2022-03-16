2d68479b2385498a74432ce79c66a2d7ba2de99b
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-03-16 06:15:12 +0530

    Container

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,10 @@ if(len(name) != 0):
 
 col1, col2 = st.columns(2);
 
-if(Semester == 1):
-    st.write("Enter your Marks!")
-    with col1:
-        with st.expander("Theory Marks"):
-            st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
+with st.container:
+    if(Semester == 1):
+        st.write("Enter your Marks!")
+        with col1:
+            with st.expander("Theory Marks"):
+                st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
 
