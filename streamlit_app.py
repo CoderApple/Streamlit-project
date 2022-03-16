f4ad08fdd1f43a378eb57e8a209e18d5006f4bad
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-03-16 06:44:56 +0530

    columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 sem = 0;
 def calc(sem):
-    
+    col1, col2 = st.columns(2);
     if sem == 1:
         with col1:
             with st.expander("Theory Marks"):
-                st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
+                first = st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
 
         with col2:
             with st.expander("Practical Marks"):
-                st.number_input("Applied Chemistry", min_value=0, max_value=100, step=1)
+                second = st.number_input("Applied Chemistry", min_value=0, max_value=100, step=1)
 
 st.markdown("<h1 style='text-align: center; color: red;'>CalcGPA</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; '>Semester GPA Calculator of B.Tech(EEE)</h3>", unsafe_allow_html=True)
@@ -21,7 +21,7 @@ with st.container():
         st.write("Hello! ", name)
         Semester = st.number_input('Semester', min_value = 0, max_value = 8, step = 1)
         sem += Semester
-    col1, col2 = st.columns(2);
+    
 
     if sem:
         st.write("")
