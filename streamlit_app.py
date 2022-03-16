304383cc1f7767acfbdbb9d640c2f4a4a0788274
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-03-17 05:20:46 +0530

    credits

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,9 +34,9 @@ def calc(sem):
     col1, col2 = st.columns(2)
 
     if sem == 1 :
-        subjects = { 'App. Maths-I' : 4, 'App. Physics-I' : 3, 'Manufacturing Processes' : 3, 'Electrical Tech.' : 3, 'Human Values' : 1, 'Fundamentals of Computing' : 2, 'App. Chemistry' : 3 }
-        labs = { 'App. Physics Lab-I' : 1, 'Elecrical Tech. Lab' : 1, 'Workshop' : 2, 'Engg. Graphics Lab' : 2, 'FOC Lab' : 1, 'App. Chemistry Lab' : 1 }
-        credits = 27
+        subjects = { 'App. Maths-I' : 4, 'App. Physics-I' : 3, 'Manufacturing Processes' : 4, 'Electrical Science' : 3, 'Communication Skills' : 3, 'App. Chemistry' : 3 }
+        labs = { 'App. Physics Lab-I' : 1, 'Elecrical Science Lab' : 1, 'Engg. Graphics Lab' : 2, 'App. Chemistry Lab' : 1 }
+        credits = 25
 
     elif sem == 2:
         subjects = { 'App. Maths-II' : 4, 'App. Physics-II' : 3, 'Electronic Devices' : 3, 'Intro To Programming' : 3, 'Engineering Mechanics' : 3, 'Communication Skills' : 3, 'Environmental Studies' : 3 }
