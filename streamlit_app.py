b1b87182bdc496343705f94921a22165d8253e66
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-03-16 06:23:42 +0530

    Container

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,24 @@
 import streamlit as st
 
-st.title("CalcGPA")
-st.header("Semester GPA Calculator for the BTech of IPU")
+with st.container:
+    st.title("CalcGPA")
+    st.header("Semester GPA Calculator for the BTech of IPU")
 
-name = st.text_input('Full Name')
-if(len(name) != 0):
-    st.write("Hello! ", name)
-    Semester = st.number_input('Semester', min_value = 0, max_value = 8, step = 1)
+    name = st.text_input('Full Name')
+    if(len(name) != 0):
+        st.write("Hello! ", name)
+        Semester = st.number_input('Semester', min_value = 0, max_value = 8, step = 1)
 
-col1, col2 = st.columns(2);
+    col1, col2 = st.columns(2);
 
-if(Semester == 1):
-    st.write("")
-    st.write("")
-    st.write("Enter your Marks!")
-    with col1:
-        with st.expander("Theory Marks"):
-            st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
+    if(Semester == 1):
+        st.write("")
+        st.write("")
+        st.write("Enter your Marks!")
+        with col1:
+            with st.expander("Theory Marks"):
+                st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
 
-    with col2:
-        with st.expander("Practical Marks"):
-            st.number_input("Applied Chemistry", min_value=0, max_value=100, step=1)
+        with col2:
+            with st.expander("Practical Marks"):
+                st.number_input("Applied Chemistry", min_value=0, max_value=100, step=1)
