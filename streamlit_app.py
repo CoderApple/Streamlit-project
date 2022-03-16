9fa3b73344d177b3ebefdffc19310faca3d581d1
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-03-16 06:32:10 +0530

    modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 sem = 0;
+def calc(sem):
+    pass
 
 st.markdown("<h1 style='text-align: center; color: red;'>CalcGPA</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; '>Semester GPA Calculator of B.Tech(EEE)</h3>", unsafe_allow_html=True)
@@ -13,14 +15,8 @@ with st.container():
         sem += Semester
     col1, col2 = st.columns(2);
 
-    if(sem == 1):
+    if sem:
         st.write("")
         st.write("")
         st.markdown("<h3 style='text-align: center; '>Enter Marks!</h3>", unsafe_allow_html=True)
-        with col1:
-            with st.expander("Theory Marks"):
-                st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
-
-        with col2:
-            with st.expander("Practical Marks"):
-                st.number_input("Applied Chemistry", min_value=0, max_value=100, step=1)
+        
