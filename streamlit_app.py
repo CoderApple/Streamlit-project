0bd73dc272dfcfc6a36a8921479a960d515c4ffb
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-03-16 06:56:07 +0530

    subjects sem-1

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,19 @@ def calc(sem):
     if sem == 1:
         with col1:
             with st.expander("Theory Marks"):
-                first = st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
+                ftfs = st.number_input("Programming in C/Applied Chemistry", min_value=0, max_value=100, step=1)
+                stss = st.number_input("Applied Physics-I", min_value=0, max_value=100, step=1)
+                ttts = st.number_input("Communication Skills/Indian Constitution/Human Values and Ethics", min_value=0, max_value=100, step=1)
+                ftfs = st.number_input("Applied Mathematics-I", min_value=0, max_value=100, step=1)
+                ftvs = st.number_input("Manufacturing Process", min_value=0, max_value=100, step=1)
+                stxs = st.number_input("Electrical Science/Environmental Studies", min_value=0, max_value=100, step=1)
 
         with col2:
             with st.expander("Practical Marks"):
-                second = st.number_input("Applied Chemistry", min_value=0, max_value=100, step=1)
+                fpfs = st.number_input("Programming in C Lab/Applied Chemistry", min_value=0, max_value=100, step=1)
+                spss = st.number_input("Physics-I Lab", min_value=0, max_value=100, step=1)
+                tpts = st.number_input("Engineering Graphics-I", min_value=0, max_value=100, step=1)
+                fpfs = st.number_input("Electrical Science Lab/Environmental Studies Lab", min_value=0, max_value=100, step=1)
 
 st.markdown("<h1 style='text-align: center; color: red;'>CalcGPA</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; '>Semester GPA Calculator of B.Tech(EEE)</h3>", unsafe_allow_html=True)
