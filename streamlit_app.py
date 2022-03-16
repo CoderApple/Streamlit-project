678f19e1ae13041491863e3565a1ea4170921652
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-03-16 05:39:02 +0530

    Semester

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ st.header("Semester GPA Calculator for the BTech of IPU")
 name = st.text_input('Full Name')
 if(len(name) != 0):
     st.write("Hello! ", name)
+    Semester = st.number_input('Semester')
